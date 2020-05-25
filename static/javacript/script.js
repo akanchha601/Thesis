a7701ae9a3c07f8e65453445fb6a24a4c0bd7ba5
akanchha601 <55644004+akanchha601@users.noreply.github.com>
2020-05-25 15:46:43 -0300

    Delete script.js

--- static/javacript/script.js
+++
@@ -1,9 +0,0 @@
-/*javascript method to call background process*/
-('#idform').on('submit', function () {
-	$.getJSON('/background_process', {
-	    domain: $('input[name="domain"]').val(),
-		}, function(data) {
-		$("#result").text(data.result);
-		});
-		return false;
-		});
